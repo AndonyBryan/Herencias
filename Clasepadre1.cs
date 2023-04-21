910513b04e9cf8059c310f2e23eacf498b322aa3
andon <andon@MATUZ>
2023-04-21 09:04:16 -0600

    Agregar archivos de proyecto.

---
+++ Clasepadre1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencias
+{
+    internal class Clasepadre1
+    { 
+public void Saludar()
+        {
+            Console.WriteLine("Hola soy: ");
+
+        }
+
+        public void Despedirse() {
+            Console.WriteLine("Asta luego");
+
+        }
+        public void Nombre() {
+        Console.WriteLine("Antonio MOntana");
+        } 
+        public void Edad()
+        {
+            Console.WriteLine("19");
+        }
+
+
+
+    
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+  
+namespace Herencias
+{
+
+    class program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            Clasehija1 Herencia = new Clasehija1();
+            Herencia.Saludar();
+            Herencia.Nombre();
+            Herencia.Edad();
+          
+            Herencia.Despedirse();
+
+           
+             
+            Console.ReadKey();
+        }
+    }
+}
